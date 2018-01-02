662de8e395877e80f08ab7b0751d724b61849a87
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2018-01-02 17:59:33 -0300

    adicionado novas funções, listar todas as pessoas.

--- main.py
+++ main.py
@@ -1,43 +1,55 @@
+
 import json
+import codecs
 
 class Pessoa:
     dados = {}
     
     def add_atribute(self):
-        """Adiçao de keys e valores para uma pessoa"""
+        """Adiçao de keys e valores para o dicionário da pessoa"""
         key = input('Digite o nome do novo atributo:').strip(' ').capitalize()
         value = input('{}:'.format(key))
-        
         dado = { key:value }
         self.dados.update(dado)
 
 class DataBank():
-    pessoas = []
+    pessoas = [] #lista de pessoas
     
     def add_pessoas(self, pessoa):
         """Adicionar objeto pessoa a lista de pessoas"""
         self.pessoas.append(pessoa.dados)
 
     def salvar_json(self):
-
+        """Exportar a lista de pessoas para um json"""
         def jdefault(o): #configura o json para receber objetos
             return o.__dict__
-        with open('data_bank.json', 'w') as data_bankjson:
+        with open('data_bank.json', 'w', encoding="utf-8") as data_bankjson:
             json.dump(self.pessoas, data_bankjson)
 
     def carregar_bank(self):
+        """Carrega os dados do json da lista de pessoas"""
         try:
             with open('data_bank.json', 'r') as databankjson:
                 self.pessoas = json.load(databankjson)
         except FileNotFoundError:
-            print('Arquivo JSON não encontrado!')
+            print('')
+            
+    def listar_pessoas(self):
+        """Listar os dados das pessoas salvas"""
+        for pessoa in self.pessoas:
+            for key, value in pessoa.items():
+                print(key+':'+value)
+        
 
 pessoas = DataBank()
+
+pessoas.carregar_bank()
+
 while(True):
         print('---MENU---')
         print('1. Adicionar uma nova pessoa.')
-        print('2. Salvar dados.')
-        print('3. Carregar dados')
+        print('2. Listar todas as pessoas.')
+        print('0. Salvar e sair.')
         
         option = input('Escolha:')
         
@@ -51,9 +63,11 @@ while(True):
                 if continua == 'N':
                     pessoas.add_pessoas(pessoa)
                     break
-                    
+        
         elif option == '2':
+            pessoas.listar_pessoas()
+                    
+        elif option == '0':
             pessoas.salvar_json()
+            break
             
-        elif option == '3':
-            pessoas.carregar_bank()
