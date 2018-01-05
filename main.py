024f56302c748b0b6726c431b4cedb863a7c503c
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2018-01-04 21:32:35 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ class Pessoa:
                 
             if continua == 'N':
                 break
+    
+
+                
+        
 
 class DataBank():
     pessoas = [] #lista de pessoas
@@ -55,10 +59,34 @@ class DataBank():
         self.listar_pessoas()
         number = int(input('Digite o numero da posição:'))
         del(self.pessoas[number])
+    
+    def att_dado(self):
+        self.listar_pessoas()
+        posicao = int(input('Digite a posição da pessoa a ser atualizada:'))
+        print('---Chaves---')
+        for i in range(len(self.pessoas)):
+            for key, value in self.pessoas[i].items():
+                print('{}: {}'.format(key, value))
+        chave = input('Digite o nome da chave a ser alterada:').capitalize()
+        
+        try:
+            self.pessoas[posicao][chave] = input('Digite o novo valor: ')
+        except:
+            print('Key not found.')
+            
+    def exp_html(self):
+        with open('pessoas.html', 'w') as people:
+            people.writelines('<meta charset="UTF-8">\n')
+            people.writelines('<h1>Pessoas</h1>\n')
+            for i in range(len(self.pessoas)):
+                people.writelines('<hr>\n')
+                people.writelines('<h3>Nome:{}</h3><br>\n'.format(self.pessoas[i]['Nome']))
+                for key, value in self.pessoas[i].items():
+                    people.writelines('<p><b>{}</b>: {}</p>\n'.format(key, value))    
         
         
-
-                
+        
+ 
 
 pessoas = DataBank()
 
@@ -68,7 +96,10 @@ while(True):
         print('---MENU---')
         print('1. Adicionar uma nova pessoa.')
         print('2. Listar todas as pessoas.')
+       
         print('3. Excluir uma pessoa.')
+        print('4. Atualizar um atributo de uma pessoa já cadatrada ou adicionar um novo.')
+        print('5. Exportar pessoas para HTML')
         print('0. Salvar e sair.')
         
         option = input('Escolha:')
@@ -77,14 +108,25 @@ while(True):
             pessoa = Pessoa()
             pessoa.add_atribute()
             pessoas.add_pessoas(pessoa)
+            pessoas.salvar_json()
  
                 
         
         elif option == '2':
             pessoas.listar_pessoas()
-        
+            
+            
         elif option == '3':
             pessoas.remov_pessoa()
+            pessoas.salvar_json()
+            
+        elif option == '4':
+            pessoas.att_dado()
+            pessoas.salvar_json()
+        
+        elif option == '5':
+            pessoas.exp_html()
+
                     
         elif option == '0':
             pessoas.salvar_json()
